fcba9e140dda2c3dd43a98241bd30dc23b14c9c0
biswasdebsubhra <biswasdebsubhra@gmail.com>
2018-03-16 06:57:36 +0000

    Initial commit

--- tasks/bbr-backup-director/task.sh
+++ tasks/bbr-backup-director/task.sh
@@ -5,17 +5,28 @@
 #bakup_time=$(date +"%F"-"%S")
 
 om_cmd curl -p /api/v0/deployed/director/credentials/bbr_ssh_credentials > bbr_keys.json
-BOSH_PRIVATE_KEY=$(jq -r '.credential.value.private_key_pem' bbr_keys.json)
+BOSH_PRIVATE_KEY=$(jq -r '.credential.value.private_key_pem' bbr_keys.json
 
-cd ../../../binary
-./bbr director --private-key-path <(echo "${BBR_PRIVATE_KEY}") --username bbr --host "${BOSH_ADDRESS}" backup-cleanup
-cd ../bbr-pipeline-tasks-repo/tasks/bbr-backup-director/
-#tar -cvf director-backup.tar -- *
+backup-timestamp=$(date +"%Y-%m-%d-%S")
+e=$?
 pushd ../../../director-backup-artifact
   ../binary/bbr director --host "${BOSH_ADDRESS}" \
   --username bbr \
   --private-key-path <(echo "${BBR_PRIVATE_KEY}") \
   backup
+  if [ "${e}" -ne "0"]; then
+    pushd ../../../director-backup-artifact
+      ../binary/bbr director --host "${BOSH_ADDRESS}" \
+      --username bbr \
+      --private-key-path <(echo "${BBR_PRIVATE_KEY}") \
+      backup-cleanup
+  fi
+  pushd ../../../director-backup-artifact
+  ../binary/bbr director --host "${BOSH_ADDRESS}" \
+  --username bbr \
+  --private-key-path <(echo "${BBR_PRIVATE_KEY}") \
+  backup
     
-  tar -cvf director-backup.tar -- *
+  tar -cvf director-backup-$backup-timestamp.tar -- *
 popd
+
--- tasks/bbr-backup-ert/task.sh
+++ tasks/bbr-backup-ert/task.sh
@@ -4,16 +4,27 @@
 
 #bakup_time=$(date +"%F"-"%S")
 
-cd ../../../binary
-./bbr deployment --target "${BOSH_ADDRESS}" --username "${BOSH_CLIENT}" --deployment "${ERT_DEPLOYMENT_NAME}" --ca-cert "${BOSH_CA_CERT_PATH}" backup-cleanup
-cd ../bbr-pipeline-tasks-repo/tasks/bbr-backup-ert/
-
+backup-timestamp=$(date +"%Y-%m-%d-%S")
+e=$?
 pushd ../../../ert-backup-artifact
   ../binary/bbr deployment --target "${BOSH_ADDRESS}" \
   --username "${BOSH_CLIENT}" \
   --deployment "${ERT_DEPLOYMENT_NAME}" \
   --ca-cert "${BOSH_CA_CERT_PATH}" \
   backup --with-manifest
-
-  tar -cvf ert-backup.tar -- *
+  if [ "${e}" -ne "0"]; then
+  	pushd ../../../ert-backup-artifact
+  ../binary/bbr deployment --target "${BOSH_ADDRESS}" \
+  --username "${BOSH_CLIENT}" \
+  --deployment "${ERT_DEPLOYMENT_NAME}" \
+  --ca-cert "${BOSH_CA_CERT_PATH}" \
+  backup-cleanup
+  fi
+  pushd ../../../ert-backup-artifact
+  ../binary/bbr deployment --target "${BOSH_ADDRESS}" \
+  --username "${BOSH_CLIENT}" \
+  --deployment "${ERT_DEPLOYMENT_NAME}" \
+  --ca-cert "${BOSH_CA_CERT_PATH}" \
+  backup --with-manifest
+  tar -cvf ert-backup-$backup-timestamp.tar -- *
 popd
