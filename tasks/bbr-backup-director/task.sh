855655915a0dcb024fcc950fff2e94e325a8b957
biswasdebsubhra <biswasdebsubhra@gmail.com>
2018-03-16 07:18:02 +0000

    missed bracket

--- tasks/bbr-backup-director/task.sh
+++ tasks/bbr-backup-director/task.sh
@@ -5,11 +5,11 @@
 #bakup_time=$(date +"%F"-"%S")
 
 om_cmd curl -p /api/v0/deployed/director/credentials/bbr_ssh_credentials > bbr_keys.json
-BOSH_PRIVATE_KEY=$(jq -r '.credential.value.private_key_pem' bbr_keys.json
+BOSH_PRIVATE_KEY=$(jq -r '.credential.value.private_key_pem' bbr_keys.json)
 
 backup-timestamp=$(date +"%Y-%m-%d-%S")
 e=$?
-pushd ../../../director-backup-artifact <<EOF
+pushd ../../../director-backup-artifact 
   ../binary/bbr director --host "${BOSH_ADDRESS}" \
   --username bbr \
   --private-key-path <(echo "${BBR_PRIVATE_KEY}") \
@@ -29,4 +29,3 @@ pushd ../../../director-backup-artifact <<EOF
     
   tar -cvf director-backup-$backup-timestamp.tar -- *
 popd
-EOF
--- tasks/bbr-backup-ert/task.sh
+++ tasks/bbr-backup-ert/task.sh
@@ -6,7 +6,7 @@
 
 backup-timestamp=$(date +"%Y-%m-%d-%S")
 e=$?
-pushd ../../../ert-backup-artifact <<EOF
+pushd ../../../ert-backup-artifact 
   ../binary/bbr deployment --target "${BOSH_ADDRESS}" \
   --username "${BOSH_CLIENT}" \
   --deployment "${ERT_DEPLOYMENT_NAME}" \
@@ -28,4 +28,3 @@ pushd ../../../ert-backup-artifact <<EOF
   backup --with-manifest
   tar -cvf ert-backup-$backup-timestamp.tar -- *
 popd
-EOF
